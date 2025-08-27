c621f80ef63c84e4c0697d6fa2915aa0decdb872
TruongTrungTin_FIVE_CAT <128278161+TruongTrungTinFIVECAT22CT113@users.noreply.github.com>
2025-08-27 13:09:13 +0700

    cập nhật 7

--- deepfake-project/check_env.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-import torchaudio
-import xformers
-
-print("Torch:", torch.__version__, "CUDA:", torch.version.cuda, "Available:", torch.cuda.is_available())
-print("Torchvision:", torchvision.__version__)
-print("Torchaudio:", torchaudio.__version__)
-print("Xformers:", xformers.__version__)
